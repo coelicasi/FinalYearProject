58b50b888351c826de242d258efd9be1575d7bdf
Alexandra McElhinney <lex.mcelhinney@gmail.com>
2016-01-18 15:23:48 +0000

    Gui working

--- FYP.py
+++ FYP.py
@@ -1,11 +1,6 @@
-from tkinter import *#
+from tkinter import *
+from tkinter import messagebox
 import nltk
-
-root = Tk()#this stays as Tk
-root.title("Text Decoder")#the title OBVIOUSLY
-root.geometry("700x500")#window size
-
-input = StringVar()
 
 class sampleText: 
 	def readSampleText():
@@ -26,9 +21,6 @@ class sampleText:
 		file_text.close()
 
 class acrostics():
-	
-	def user_input():
-		text = GUI.entry.get(1.0, END)
 		print(text)
 	def start_of_sentence():
 		for i in range(len(words)):
@@ -37,32 +29,45 @@ class acrostics():
 		for i in range(len(words)):
 				end_of_line.append(words[i].split()[-1:])
 
-class GUI:
+class App:
 
-	app = Frame(root)
-	app.pack()
+	def changeLabel(self):
+			text = "You input: " + self.user.get()
+			self.labelText.set(text)
+			self.user.delete(0, END)
 
-	instructions = Label(root, text = "Type in your text to be decoded or try a sample text").pack(side = "top")
+	def __init__(self, master):
+		frame = Frame(master)
+		frame.pack()
+		self.button = Button(frame, text="QUIT", fg="red", command=frame.quit)
+		self.button.pack(side='top')
 
-	entry = Text(root, height = 5).pack()
+		self.hi_there = Button(frame, text="OK", command=self.changeLabel)
+		self.hi_there.pack(side= 'top', padx = 10)
 
-	def retrieve_input():
-		text = entry.get(1.0, END)
-		Label(root, text = text).pack(side = "top")
+		self.acrosticsb = Button(frame, text = "Acrostics", command = acrostics.user_input)
+		self.acrosticsb.pack()
 
-	
-	acrosticsb = Button(app, text = "Acrostics", command = acrostics.user_input).pack(side = "left", padx = 10, pady = 10)
-	numeralsb = Button(app, text="Numerals").pack(side = "left", padx = 10, pady = 10)
-	ratiosb = Button(app, text = "Ratios").pack(side = "left", padx = 10, pady = 10)
-	sampletextb = Button(app, text = "Sample", command = sampleText.readSampleText).pack(side = "right", padx = 50, pady = 10)
+		self.numeralsb = Button(frame, text="Numerals")
+		self.numeralsb.pack()
 
-	display = Button(app, text = "display", command = retrieve_input).pack()
+		self.ratiosb = Button(frame, text = "Ratios")
+		self.ratiosb.pack()
 
-	
-	
-	#user = entry.get(1.0, END)
-	#print(user)
-	root.mainloop()
+		self.sampletextb = Button(frame, text = "Sample", command = sampleText.readSampleText)
+		self.sampletextb.pack()
+
+		self.labelText = StringVar()
+		self.labelText.set("Click OK when ready")
+		label1 = Label(frame, textvariable = self.labelText)
+		label1.pack()
+		userInput = StringVar(None)
+		self.user = Entry(frame, textvariable = userInput)
+		self.user.pack(side = "bottom")
 
 
-GUI
+
+root = Tk()
+app = App(root)
+root.geometry("700x400")
+root.mainloop()
