874dfa37ac93c20338e1dd9fc30ce74c7aa2a14a
Alexandra McElhinney <lex.mcelhinney@gmail.com>
2016-01-28 13:49:57 +0000

    Update with GUI, numberals

--- FYP.py
+++ FYP.py
@@ -15,11 +15,16 @@ class sampleText:
 			for i in range(len(words)):
 				end_of_line.append(words[i].split()[-1:])
 
+			Label(root, text = "Original Text: ", justify = "left").pack(side = "left")
 			Label(root, text = fileLines, justify = "left").pack(side = "left")
-			Label(root, text = start_of_line, wraplength = 300, justify  = "left"). pack(side="right")
-			Label(root, text = end_of_line, wraplength = 300, justify = "left").pack(side = "right")
-		file_text.close()
 
+			Label(root, text = "Start of line: ").pack()
+			Label(root, text = start_of_line, wraplength = 200, justify  = "left"). pack()
+
+			Label(root, text = "End of line: ").pack()
+			Label(root, text = end_of_line, wraplength = 200, justify = "left").pack()
+		file_text.close()
+"""
 class acrostics():
 		print(text)
 	def start_of_sentence():
@@ -28,9 +33,23 @@ class acrostics():
 	def end_of_line():
 		for i in range(len(words)):
 				end_of_line.append(words[i].split()[-1:])
+"""
+
+class Numerals:
+	
+	def readSampleText():
+		nums = []
+		with open('text.txt') as file_text:
+			lines = file_text.read()
+			tokens = lines.split()
+			for token in tokens:
+				if token.isdigit():
+					for i in reversed(xrange(token)):
+						nums.append(token)
+		print(i)			
+			
 
 class App:
-
 	def changeLabel(self):
 			text = "You input: " + self.user.get()
 			self.labelText.set(text)
@@ -39,23 +58,6 @@ class App:
 	def __init__(self, master):
 		frame = Frame(master)
 		frame.pack()
-		self.button = Button(frame, text="QUIT", fg="red", command=frame.quit)
-		self.button.pack(side='top')
-
-		self.hi_there = Button(frame, text="OK", command=self.changeLabel)
-		self.hi_there.pack(side= 'top', padx = 10)
-
-		self.acrosticsb = Button(frame, text = "Acrostics", command = acrostics.user_input)
-		self.acrosticsb.pack()
-
-		self.numeralsb = Button(frame, text="Numerals")
-		self.numeralsb.pack()
-
-		self.ratiosb = Button(frame, text = "Ratios")
-		self.ratiosb.pack()
-
-		self.sampletextb = Button(frame, text = "Sample", command = sampleText.readSampleText)
-		self.sampletextb.pack()
 
 		self.labelText = StringVar()
 		self.labelText.set("Click OK when ready")
@@ -63,9 +65,32 @@ class App:
 		label1.pack()
 		userInput = StringVar(None)
 		self.user = Entry(frame, textvariable = userInput)
-		self.user.pack(side = "bottom")
+		self.user.pack(side = 'top', padx = 20, pady = 30)
 
 
+		self.button = Button(frame, text="QUIT", fg="red", command=frame.quit)
+		self.button.pack(side='bottom')
+
+		self.hi_there = Button(frame, text="OK", command=self.changeLabel)
+		self.hi_there.pack(side= 'bottom', padx = 10)
+
+		self.acrosticsb = Button(frame, text = "Acrostics")
+		self.acrosticsb.pack(side = 'left', padx = 10, pady = 10)
+
+		self.numeralsb = Button(frame, text="Numerals")
+		self.numeralsb.pack(side = 'left', padx = 10, pady = 10)
+
+		self.ratiosb = Button(frame, text = "Ratios")
+		self.ratiosb.pack(side = 'left', padx = 10, pady = 10)
+
+		self.sampletextb = Button(frame, text = "Sample Acrostics", command = sampleText.readSampleText)
+		self.sampletextb.pack(side = 'left', padx = 10, pady = 10)
+
+		self.sampletextb = Button(frame, text = "Sample Numerals", command = Numerals.readSampleText)
+		self.sampletextb.pack(side = 'left', padx = 10, pady = 10)
+
+
+		
 
 root = Tk()
 app = App(root)
