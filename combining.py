7a65fdea4ac378b87cdce47f4da9244191e92408
Alexandra McElhinney <lex.mcelhinney@gmail.com>
2016-04-01 20:31:39 +0100

    ordered words, added to samples

--- combining.py
+++ combining.py
@@ -1,9 +1,11 @@
 import string
 from word2number import w2n
+from collections import defaultdict
+
+dictionary = defaultdict(list)
 ##probably bad coding practice, set returns, populate later?
 wordList = []
 charlist = []
-indexes = []
 numbers = []
 start_of_line = []
 end_of_line = []
@@ -16,30 +18,59 @@ prichararray = []
 
 def readFiles():
     try:
-        with open('C:\\Users\\Alexandra\\Documents\\College\\FinalYearProject\\FinalYearProject\\pattern.txt') as file_text:  # reading in a file
+        with open('C:\\Users\\Alexandra\\Documents\\College\\FinalYearProject\\FinalYearProject\\moreNumbers.txt') as file_text:  # reading in a file
             fileLines = file_text.read()  # read method
             return fileLines
     except IOError:
         print("File could not be read. It either does not exist or does not have read access")
+'''
+def indexdictionary(fileLines):
+    textlines = fileLines.replace("-", " ").translate(str.maketrans("", "", string.punctuation)).split()
+    for i in range(len(textlines)):
+        dictionary[textlines[i]].append(i+1)'''
 
 
-
-def breaktext(fileLines):
+def charactertext(fileLines):
     line = fileLines.replace("-", " ")
     lines = fileLines.translate(str.maketrans("", "", string.punctuation)).replace(" ", "")
-    newlines = [x.strip("\n") for x in lines]
-    return newlines
+    characters = [x.strip("\n") for x in lines]
+    return characters#returns characters
+
+def reversetext(fileLines):
+    reverse_text = []
+    textlines = fileLines.replace("-", " ").translate(str.maketrans("", "", string.punctuation)).split()
+    for i in range(len(textlines)-1, -1, -1):#REVERSE REVERSE
+        reverse_text.append(textlines[i])
+    return reverse_text
+
 
 def reverseChar(fileLines):
-    reversechar = fileLines[::-1]
+    reversechar = fileLines.replace("-", " ")
     lines = reversechar.translate(str.maketrans("", "", string.punctuation)).replace(" ", "")
     reversetokens = [x.strip("\n") for x in lines]
     return reversetokens
 
-def numerals(fileLines, newlines, reversetokens):
+def numbers(fileLines, numwordlist):
+    index = []
+    textlines = fileLines.translate(str.maketrans("", "", string.punctuation)).split()
+
+    for i in range(len(textlines)):
+        try:
+            index.append(int(textlines[i]))
+        except:
+            pass
+
+    for i in range(len(numwordlist)):
+        try:
+            index.append(int(numwordlist[i]))
+        except: pass
+
+    return index
+
+def numerals(fileLines, characters , reversetokens):
     wordrev = ' '.join(fileLines.split()[::-1])
     wordreverse = list(wordrev.split())
-    textlines = fileLines.split()
+    textlines = fileLines.translate(str.maketrans("", "", string.punctuation)).split()
     paragraphs = fileLines.split(("\n"))
     sentences = fileLines.split(".")
 
@@ -61,7 +92,7 @@ def numerals(fileLines, newlines, reversetokens):
     #n character from the start and end
     for token in range(len(textlines)):
         try:
-            charlist.append(newlines[int(textlines[token])-1])#START
+            charlist.append(characters [int(textlines[token])-1])#START
             charlist.append(reversetokens[int(textlines[token])-1])#END
         except: pass
     #first word of the nth sentence
@@ -77,9 +108,8 @@ def numerals(fileLines, newlines, reversetokens):
         except: pass
 
 
-
 def convertNumbers(fileLines):
-    textlines = fileLines.split()
+    textlines = fileLines.translate(str.maketrans("", "", string.punctuation)).split()
     numwordlist = []
 
     for i in range(len(textlines)):
@@ -91,10 +121,10 @@ def convertNumbers(fileLines):
     return numwordlist
 
 
-def byWord(numwordlist, fileLines, newlines, reversetokens):
+def byWord(numwordlist, fileLines, characters, reversetokens):
     wordrev = ' '.join(fileLines.split()[::-1])
     wordreverse = list(wordrev.split())
-    textlines = fileLines.split()
+    textlines = fileLines.translate(str.maketrans("", "", string.punctuation)).split()
     paragraphs = fileLines.split(("\n"))
     sentences = fileLines.split(".")
 
@@ -106,7 +136,7 @@ def byWord(numwordlist, fileLines, newlines, reversetokens):
             pass
         #characters from start/end
         try:
-            charlist.append(newlines[numwordlist[i] - 1])
+            charlist.append(characters[numwordlist[i] - 1])
             charlist.append(reversetokens[numwordlist[i] - 1])
         except:
             pass
@@ -167,8 +197,8 @@ def fibList(c):
         fList.append(Fibonacci(i + 1))
     return fList
 
-def fibCheck(fList, fileLines, newlines):
-    textlines = fileLines.split()
+def fibCheck(fList, fileLines, characters):
+    textlines = fileLines.translate(str.maketrans("", "", string.punctuation)).split()
     for i in range(len(fList)):
         fList[i] = fList[i] - 1
     try:
@@ -179,17 +209,17 @@ def fibCheck(fList, fileLines, newlines):
 
     try:
         for i in range(len(fList)):
-            fibchararry.append(newlines[fList[i]])
+            fibchararry.append(characters[fList[i]])
     except IndexError:
         print("Array out of bounds!")
 
-def primeCheck(primeList, fileLines, newlines):
-    textlines = fileLines.split()
+def primeCheck(primeList, fileLines, characters):
+    textlines = fileLines.translate(str.maketrans("", "", string.punctuation)).split()
     for i in range(len(primeList)):
         primeList[i] = primeList[i] - 1
     try:
         for i in range(len(primeList)):
-            prichararray.append(newlines[primeList[i]])
+            prichararray.append(characters[primeList[i]])
     except IndexError:
         print("Array out of bounds!")
 
@@ -199,11 +229,35 @@ def primeCheck(primeList, fileLines, newlines):
     except IndexError:
         print("Array out of bounds!")
 
+def crossdiscovery(fList, fileLines, index):
+    textlines = fileLines.translate(str.maketrans("", "", string.punctuation)).split()
+    cross = list(set(fList).intersection(index))
+    crossed_list = []
+    for i in range(len(cross)):
+        crossed_list.append(textlines[cross[i] - 1])
+
+    return crossed_list
+
+def check(fileLines):
+    textlines = fileLines.translate(str.maketrans("", "", string.punctuation)).split()
+    secretcode = []
+    for word in range(len(textlines)):
+        for w in range(len(wordList)):
+            if textlines[word] == wordList[w]:
+                #print(word+1, wordList[w])
+                secretcode.append(wordList[w])
+    return secretcode
 
 def main():
-    numerals(readFiles(), breaktext(readFiles()), reverseChar(readFiles()))
-    convertNumbers(readFiles())
-    byWord(convertNumbers(readFiles()), readFiles(), breaktext(readFiles()), reverseChar(readFiles()))
+    numerals(readFiles(), charactertext(readFiles()), reverseChar(readFiles()))
+    #convertNumbers(readFiles())
+    #byWord(convertNumbers(readFiles()), readFiles(), breaktext(readFiles()), reverseChar(readFiles()))
+    #crossdiscovery(fibList(10),readFiles())
+    #print(crossdiscovery(fibList(10), readFiles(), numbers(readFiles(), convertNumbers(readFiles()))))
 
+    #indexdictionary(readFiles())
+    #searchDictionary(readFiles())
+
+    print(check(readFiles()))
 
 main()
